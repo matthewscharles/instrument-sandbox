d8d624d4e6a0175b2c9a0b5a3824aee88803267c
Charles Matthews <charles0matthews@gmail.com>
2023-09-21 08:54:59 -0400

    add midi, address touch issues

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import * as dat from "dat.gui";
 import MultitouchMapper from "@matthewscharles/multitouch-mapper";
 import KeyboardMapper from "keyboard-mapper";
+import MidiMapper from "midi-mapper";
 
 window.dat = dat;
 window.touch = new MultitouchMapper();
 window.keyboardMapper = new KeyboardMapper(false);
+window.midi = new MidiMapper();
