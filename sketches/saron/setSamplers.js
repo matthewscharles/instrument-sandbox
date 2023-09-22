4e3f27336a21d3af923b9fb1222245e9ce69dbdb
Charles Matthews <charles0matthews@gmail.com>
2023-09-22 15:56:32 -0400

    some basic slendro for the demo

--- sketches/saron/setSamplers.js
+++ sketches/saron/setSamplers.js
@@ -1,6 +1,19 @@
+let tuning = {
+    slendro:{
+        "M1":0.73,
+        "M2":0.83,
+        "M3":0.96,
+        "M5":1.09,
+        "M6":1.27
+    }
+}
+
+tuning.slendro.H1 = tuning.slendro.M1*2;
+
 export default function setSamplers(){
     window.players = [...new Array(6)].map(()=>new Tone.Player(`sounds/H3_lo.mp3`).toDestination());
     window.players.forEach((x,i)=>{
-        x.playbackRate = 0.5 + (i/3);
+        
+        x.playbackRate = Object.values(tuning.slendro)[i];
     })
 }
