c6d103f0736da2ca961c6a394f2c976b9cd5e9d0
Charles Matthews <charles0matthews@gmail.com>
2023-09-25 15:13:05 -0400

    move to individual svg folder and accomodate in js

--- sketches/saron/replaceSvgObject.js
+++ sketches/saron/replaceSvgObject.js
@@ -1,5 +1,6 @@
 /**
  * If object is not loaded, try again every 100ms
+ * Match folders if not in root (basic implementation)
  * @param {HTMLElement} objectElement 
  */
 
@@ -15,13 +16,27 @@ function replaceSvgObject(objectElement) {
     
     if(window.verbose) console.log(`svg loaded from ${objectElement.id}`);
     
+    // console.log('data',objectElement.getAttribute('data'))
+    
+    
+    let folder = objectElement.getAttribute('data').split('/');
+    if(folder.length>1){
+        folder.pop(); 
+        console.log('folder',folder)
+        newSvgElement.querySelectorAll('image').forEach(image=>{
+            let href = image.getAttribute('href') || image.getAttribute('xlink:href');
+            image.removeAttribute('xlink:href');
+            image.setAttribute('href', `${folder}/${href}`);
+        });
+    }
+    
     objectElement.classList.forEach(className => {
         newSvgElement.classList.add(className);
     });
     
     newSvgElement.classList.replace('svg-import', 'svg-imported');
-    
-    newSvgElement.id = objectElement.id;
+    console.log('classes',newSvgElement.getAttribute('class'))
+    // newSvgElement.id = objectElement.classList.entries.join('_');
     objectElement.replaceWith(newSvgElement);
 }
 
