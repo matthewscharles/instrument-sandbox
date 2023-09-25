d787541819f16706f8f2bc170db0e3b5c352fadc
Charles Matthews <charles0matthews@gmail.com>
2023-09-25 12:13:53 -0400

    restructure, symantics

--- sketches/saron/setLaras.js
+++ sketches/saron/setLaras.js
@@ -3,11 +3,12 @@ import transitionDisplay from "./transitionOut.js";
 const setLaras=function(laras){
     window.laras = laras;
     let oldLaras = (laras=='slendro')?'pelog':'slendro';
-    document.querySelectorAll(`.${laras}.switchable`).forEach((element)=>{
-        transitionDisplay(element,true);
-    });
     document.querySelectorAll(`.${oldLaras}.switchable`).forEach((element)=>{
         transitionDisplay(element,false);
+    });
+    
+    document.querySelectorAll(`.${laras}.switchable`).forEach((element)=>{
+        transitionDisplay(element,true);
     });
 }
 
