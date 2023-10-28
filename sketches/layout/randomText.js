09edd31ab39076c8f74b97b8fe475ad79f730a0a
Charles Matthews <charles0matthews@gmail.com>
2023-10-28 11:11:13 -0400

    layout template improvements

---
+++ sketches/layout/randomText.js
@@ -0,0 +1,25 @@
+function randomText(){
+    
+}
+
+let content = document.querySelector('#message');
+    let message = 'the quick brown fox jumps over the lazy dog';
+    let scrambledMessage = scramble(message);
+    for(let i=0;i<1000;i++) {
+        scrambledMessage = scramble(scrambledMessage);
+        content.innerHTML += scrambledMessage + '<br>';
+    }
+    
+    
+    function scramble(message) {
+        let scrambled = '';
+        let chars = message.split('');
+        while(chars.length > 0) {
+            let randomIndex = Math.floor(Math.random() * chars.length);
+            scrambled += chars[randomIndex];
+            chars.splice(randomIndex, 1);
+        }
+        return scrambled;
+    }
+    
+export default randomText;
--- sketches/layout/window.js
+++ sketches/layout/window.js
@@ -1,23 +1,7 @@
+import randomText from './randomText.js';
+
 window.addEventListener('load',()=>{
-    let content = document.querySelector('#message');
-    let message = 'the quick brown fox jumps over the lazy dog';
-    let scrambledMessage = scramble(message);
-    for(let i=0;i<1000;i++) {
-        scrambledMessage = scramble(scrambledMessage);
-        content.innerHTML += scrambledMessage + '<br>';
-    }
-    
-    
-    function scramble(message) {
-        let scrambled = '';
-        let chars = message.split('');
-        while(chars.length > 0) {
-            let randomIndex = Math.floor(Math.random() * chars.length);
-            scrambled += chars[randomIndex];
-            chars.splice(randomIndex, 1);
-        }
-        return scrambled;
-    }
+   randomText();
 })
         
 window.addEventListener('resize',()=>{
