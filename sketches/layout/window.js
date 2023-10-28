61a08b2d779c55674d219a510d98669ad3f3c032
Charles Matthews <charles0matthews@gmail.com>
2023-10-28 12:29:28 -0400

    Update window.js

--- sketches/layout/window.js
+++ sketches/layout/window.js
@@ -4,9 +4,11 @@ window.addEventListener('load',()=>{
    randomText();
    document.querySelector('.modal__close').addEventListener('click',()=>{
          $('.modal').hide(1000);
+         $('.main').show(1000);
     });
     document.querySelector('.modal__open').addEventListener('click',()=>{
         $('.modal').show(1000);
+        $('.main').hide(1000);
     });
 })
         
