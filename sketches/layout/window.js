5b337dd7ea15137e4f3bf67ad82f68ee8de22acc
Charles Matthews <charles0matthews@gmail.com>
2023-10-28 11:32:31 -0400

    testing modal layout tempalte

--- sketches/layout/window.js
+++ sketches/layout/window.js
@@ -2,8 +2,17 @@ import randomText from './randomText.js';
 
 window.addEventListener('load',()=>{
    randomText();
+   document.querySelector('.modal__close').addEventListener('click',()=>{
+         $('.modal').hide(1000);
+    });
+    document.querySelector('.modal__open').addEventListener('click',()=>{
+        $('.modal').show(1000);
+    });
 })
         
 window.addEventListener('resize',()=>{
     document.querySelector('.modal__content').style.marginTop = document.querySelector('.modal__banner').offsetHeight + 10 + 'px';
 })
+
+
+
