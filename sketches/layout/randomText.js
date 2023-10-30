beea9ec85396b832c85090bb7110f62c531df0cb
Charles Matthews <charles0matthews@gmail.com>
2023-10-30 17:26:12 -0400

    quick tidy up of layout template

--- sketches/layout/randomText.js
+++ sketches/layout/randomText.js
@@ -1,25 +1,23 @@
-function randomText(){
+function randomText(numberOfLines = 1000, text = 'the quick brown fox jumps over the lazy dog'){
     let content = document.querySelector('#message');
-    let message = 'the quick brown fox jumps over the lazy dog';
+    let message = text;
     let scrambledMessage = scramble(message);
-    for(let i=0;i<1000;i++) {
+    for(let i=0;i<numberOfLines;i++) {
         scrambledMessage = scramble(scrambledMessage);
         content.innerHTML += scrambledMessage + '<br>';
     }
 }
 
-
     
-    
-    function scramble(message) {
-        let scrambled = '';
-        let chars = message.split('');
-        while(chars.length > 0) {
-            let randomIndex = Math.floor(Math.random() * chars.length);
-            scrambled += chars[randomIndex];
-            chars.splice(randomIndex, 1);
-        }
-        return scrambled;
+function scramble(message) {
+    let scrambled = '';
+    let chars = message.split('');
+    while(chars.length > 0) {
+        let randomIndex = Math.floor(Math.random() * chars.length);
+        scrambled += chars[randomIndex];
+        chars.splice(randomIndex, 1);
     }
+    return scrambled;
+}
     
 export default randomText;
--- sketches/layout/window.js
+++ sketches/layout/window.js
@@ -3,7 +3,9 @@ import randomText from './randomText.js';
 window.addEventListener('load',()=>{
    randomText();
    document.querySelector('.modal__close').addEventListener('click',()=>{
-        // contents of these containers must be set to position: absolute or fixed to avoid tacky animations
+        // the contents of these containers must be set to [position: absolute] or [position: fixed]
+        // i.e. sub-containers are needed
+        // to avoid tacky animations
          $('.modal').hide(1000);
          $('.main').show(1000);
     });
