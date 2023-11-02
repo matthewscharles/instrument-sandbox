9988595e41e3a127f3229363040cbb5c4584ac46
Charles Matthews <charles0matthews@gmail.com>
2023-11-01 21:37:19 -0400

    testing grid layout template

--- sketches/layout/randomText.js
+++ sketches/layout/randomText.js
@@ -1,7 +1,8 @@
-function randomText(numberOfLines = 1000, text = 'the quick brown fox jumps over the lazy dog'){
+function randomText(numberOfLines = 1000, text = 'The quick brown fox jumped over the lazy dog.'){
     let content = document.querySelector('#message');
     if(!content) return;
     let message = text;
+    message += message + message;
     let scrambledMessage = scramble(message);
     for(let i=0;i<numberOfLines;i++) {
         scrambledMessage = scramble(scrambledMessage);
