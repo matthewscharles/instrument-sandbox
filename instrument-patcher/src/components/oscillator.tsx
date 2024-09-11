7e706012af47db482dcb7ac4e54a3382f949bb1f
Charles Matthews <charles0matthews@gmail.com>
2024-09-11 14:38:52 -0400

    add number box and handles

--- instrument-patcher/src/components/oscillator.tsx
+++ instrument-patcher/src/components/oscillator.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { NodeProps } from 'reactflow'
+import { NodeProps, Handle, Position } from 'reactflow'
 import { Box, Text } from "@chakra-ui/react";
+import { useCallback, useState } from 'react';
+
 
 export function OscillatorInit({
-            data:{ frequency },
-        }:NodeProps<{ frequency: number }>) {
+            id,
+            data:{ frequency, label },
+        }:NodeProps<{ frequency: number, label:string }>) {
+            const [number, setNumber] = useState(0);
+
+
+            const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+                const cappedNumber = Math.round( Math.min(100, Math.max(0, Number(e.target.value))));
+                setNumber(cappedNumber);
+            }, []);
     return (
         <Box bg="white" border="1px solid gray">
             <Box bg="beige">
@@ -14,9 +24,20 @@ export function OscillatorInit({
             </Box>
             <Box p={2} bg="white">
                 <Text fontSize="small" color="black">
-                    frequency: {frequency}
+                    frequency: 
+                    <input
+                        id={`number-${id}`} 
+                        onChange={onChange} 
+                        name="number"
+                        type="number" 
+                        value={frequency} 
+                        min="0" 
+                        max="255" 
+                        className="nodrag"></input>
                 </Text>
             </Box>
+            <Handle type="source" className="handle" position={Position.Bottom} />
+            <Handle type="source" className="handle" position={Position.Bottom} />
         </Box>
     )
 }
--- instrument-patcher/src/initial.tsx
+++ instrument-patcher/src/initial.tsx
@@ -4,21 +4,21 @@ export const initialNodes : Node[] = [
   {
     id: '1',
     type: 'oscillator',
-    position: { x: 250, y: 25 },
-    data: { frequency: 440 },
+    position: { x: 250, y: 0 },
+    data: { label: "oscillator", frequency: 440 },
   },
   // you can also pass a React component as a label
   {
     id: '2',
     type: 'output',
     data: { label: <div>output</div> },
-    position: { x: 250, y: 100 },
+    position: { x: 250, y: 150 },
   },
   {
     id: '3',
     type: 'filter',
     data: { label: <div>filter</div> },
-    position: { x: 300, y: 100 },
+    position: { x: 300, y: 200 },
   },
   // { id: 'e1-2', source: '1', target: '2', animated: true },
 ]
