5edafe5c0d05c9bbff646dbe6a0e8c188a311897
Charles Matthews <charles0matthews@gmail.com>
2024-09-12 10:48:18 -0400

    Create webaudiotest.js

---
+++ web-audio/webaudiotest.js
@@ -0,0 +1,44 @@
+const context = new AudioContext();
+const osc = context.createOscillator();
+const amp = context.createGain();
+
+osc.type = 'sawtooth';
+// middle C
+osc.frequency.value = 261.625565;
+// modulate in cents: one octave down
+osc.detune.value = -1200;
+
+osc.start();
+let filter = context.createBiquadFilter();
+osc.connect(filter);
+filter.connect(amp);
+amp.connect(context.destination);
+
+// inputs will modulate the existing value
+amp.gain.value=0;
+
+const lfo = context.createOscillator();
+const lfoGain = context.createGain();
+lfo.frequency.value = 0.1;
+lfo.connect(lfoGain);
+lfoGain.gain.value=0.5;
+lfoGain.connect(amp.gain);
+lfo.start();
+
+const freqLfo = context.createOscillator();
+freqLfo.frequency.value = 3;
+const freqLfoGain = context.createGain();
+freqLfo.start();
+freqLfo.connect(freqLfoGain);
+
+
+freqLfoGain.gain.value=300
+freqLfoGain.connect(filter.frequency)
+
+
+// freqLfoGain.connect(osc.frequency);
+
+// const freqBase = context.createConstantSource();
+// freqBase.offset.value = 400;
+// freqBase.connect(osc.frequency);
+// freqLfoGain.connect(freqBase.offset);
