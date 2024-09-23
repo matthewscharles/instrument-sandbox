69515a58bb7f81acf4a0b48ce7e621ced60abb09
Charles Matthews <charles0matthews@gmail.com>
2024-09-23 17:15:09 -0400

    develop web audio demo with s&h

---
+++ web-audio/sh.js
@@ -0,0 +1,40 @@
+class SampleHold extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.lastSample = 0;
+    this.prevTrigger = 0;
+  }
+
+  process(inputs, outputs, parameters) {
+    const sourceInputs = inputs[0];
+    const triggerInputs = inputs[1];
+    const outputsChannel = outputs[0];
+
+    // Ensure both inputs are connected
+    if (sourceInputs.length === 0 || triggerInputs.length === 0) {
+      // If there's no input, output 0
+      outputsChannel[0].fill(this.lastSample || 0);
+      return true;
+    }
+
+    const source = sourceInputs[0];   // source signal (mono)
+    const trigger = triggerInputs[0]; // trigger signal (mono)
+    const output = outputsChannel[0];
+
+    for (let i = 0; i < output.length; i++) {
+      const currentTrigger = trigger[i];
+
+      // Detect rising edge in trigger signal
+      if (this.prevTrigger <= 0 && currentTrigger > 0) {
+        this.lastSample = source[i];
+      }
+      
+      output[i] = this.lastSample;
+      this.prevTrigger = currentTrigger;
+    }
+    
+    return true;
+  }
+}
+
+registerProcessor('sample-hold', SampleHold);
--- web-audio/webaudiotest.js
+++ web-audio/webaudiotest.js
@@ -1,3 +1,12 @@
+/**
+ * context.currentTime + x
+ * exponentialRampToValueAtTime
+ * linearRampToValueAtTime
+ * setTargetAtTime
+ * setValueAtTime
+ * setValueCurveAtTime
+ */
+
 const context = new AudioContext();
 const osc = context.createOscillator();
 const amp = context.createGain();
@@ -5,18 +14,18 @@ const amp = context.createGain();
 const middleC = 261.625565;
 
 osc.type = 'sawtooth';
-// middle C
 osc.frequency.value = middleC;
 // modulate in cents: one octave down
 osc.detune.value = -1200;
-
 osc.start();
+
 let filter = context.createBiquadFilter();
+
 osc.connect(filter);
 filter.connect(amp);
 amp.connect(context.destination);
 
-// inputs will modulate the existing value
+// inputs modulate the existing value
 amp.gain.value=0;
 
 const lfo = context.createOscillator();
@@ -33,10 +42,82 @@ const freqLfoGain = context.createGain();
 freqLfo.start();
 freqLfo.connect(freqLfoGain);
 
-
 freqLfoGain.gain.value=300
 freqLfoGain.connect(filter.frequency)
 
+const delayUnit = {
+    delay: context.createDelay(8),
+    feedback: context.createGain(),
+    wet: context.createGain()
+}
+
+delayUnit.delay.delayTime.value = 0.5;
+delayUnit.feedback.gain.value = 0.1;
+
+delayUnit.wet.gain.value = 0.5;
+delayUnit.delay.connect(delayUnit.feedback);
+delayUnit.feedback.connect(delayUnit.delay);
+delayUnit.wet.connect(context.destination);
+delayUnit.delay.connect(delayUnit.wet);
+
+filter.connect(delayUnit.delay);
+    
+(async () => {
+    await context.audioWorklet.addModule('noise.js');
+    await context.audioWorklet.addModule('sh.js');
+    
+    class SampleAndHoldNode {
+        constructor(context) {
+          this.context = context;
+          this.node = new AudioWorkletNode(context, 'sample-hold', {
+            numberOfInputs: 2,
+            numberOfOutputs: 1,
+            channelCount: 1, // Mono processing
+          });
+    
+          // Input proxies
+          this.source = new GainNode(context);
+          this.trigger = new GainNode(context);
+    
+          // Connect the proxies to the processor inputs
+          this.source.connect(this.node, 0, 0);  // Connect to input 0
+          this.trigger.connect(this.node, 0, 1); // Connect to input 1
+    
+          // Expose the output of the processor
+          this.output = this.node;
+        }
+    
+        connect(destination) {
+          this.output.connect(destination);
+        }
+    
+        disconnect() {
+          this.output.disconnect();
+        }
+    }
+  
+    window.sh = new SampleAndHoldNode(context);
+
+    window.noise = new AudioWorkletNode(context, 'white-noise');
+
+    window.oscillator = new OscillatorNode(context, {
+        type: 'square',
+        frequency: 10, 
+    });
+
+    oscillator.start();
+
+    noise.connect(sh.source);
+    sh.connect(oscillator.frequency);
+    // sh.connect(delayUnit.delay.delayTime);
+    oscillator.connect(sh.trigger);
+    const noiseAmp = context.createGain();
+    noiseAmp.gain.value = 100;
+
+    sh.connect(noiseAmp);
+
+    noiseAmp.connect(osc.frequency);
+  })();
 
 // freqLfoGain.connect(osc.frequency);
 
