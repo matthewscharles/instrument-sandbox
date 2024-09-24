80f8865c8e22909cc79a7d632f6d6c52e2a304f1
Charles Matthews <charles0matthews@gmail.com>
2024-09-24 11:24:04 -0400

    Create noise-processor.js

---
+++ web-audio/nodes/noise-processor.js
@@ -0,0 +1,19 @@
+class NoiseProcessor extends AudioWorkletProcessor {
+  process(inputs, outputs, parameters) {
+    const output = outputs[0];
+    const channelCount = output.length;
+
+    for (let channel = 0; channel < channelCount; channel++) {
+      const outputChannel = output[channel];
+
+      for (let i = 0; i < outputChannel.length; i++) {
+        // outputChannel[i] = Math.random() * 2 - 1; // Generate white noise
+        outputChannel[i] = Math.random() * 1; // range 0-1
+      }
+    }
+
+    return true; // Keep the processor alive
+  }
+}
+
+registerProcessor('noise-processor', NoiseProcessor);
